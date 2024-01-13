94b07d7a3dd8df7a99967ffaa4dcff200284c01f
root <root@localhost.localdomain>
2024-01-13 12:29:34 +0000

    use result

--- Assignment1/codes/fig.py
+++ Assignment1/codes/fig.py
@@ -1,27 +1,17 @@
-import numpy as np
+mport numpy as np
 import matplotlib.pyplot as plt
 
-# Define the step function as a discrete signal
-def step_function(n):
-    return np.piecewise(n, [n < 0, n >= 0], [0, lambda n: 8 - 6 * n])
+def graph(n):
+        return np.piecewise(n, [n < 0, n >= 0], [0, lambda n: 8 - 6 * n])
 
-# Generate discrete values for n
-n_values_discrete = np.arange(-10, 10, 1)
+    n_values_discrete = np.arange(-10, 10, 1)
 
-# Calculate corresponding y values
-y_values_discrete = step_function(n_values_discrete)
+    y_values_discrete = graph(n_values_discrete)
 
-# Plot the stem graph
-plt.stem(n_values_discrete, y_values_discrete, label='8 - 6n (discrete signal)')
-
-# Add labels and title
-plt.xlabel('n')
-plt.ylabel('x(n)')
-plt.title('Discrete Signal of 8 - 6n using a step function')
-
-# Add a legend
-plt.legend()
-
-# Show the plot
-plt.grid(True)
-plt.savefig('Figure_1.png')
+    plt.stem(n_values_discrete, y_values_discrete, label="x(n) = 8 - 6n")
+    plt.xlabel("n")
+    plt.ylabel("x(n)")
+    plt.title("x(n) = 8 - 6n")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
